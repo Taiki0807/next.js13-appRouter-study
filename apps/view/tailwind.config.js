30185ffc472438f4cc09d254323c5f517e1e60ba
Taiki0807 <hosokawataiki265@gmail.com>
2023-06-13 17:50:44 +0900

    :wrench: Tailwind CSSカスタマイズ

--- apps/view/tailwind.config.js
+++ apps/view/tailwind.config.js
@@ -1,6 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
+  },
   plugins: [],
 };
