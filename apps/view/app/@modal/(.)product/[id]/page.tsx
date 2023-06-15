8514cfb2964f7a58001d2d47bad146bf77589500
Taiki0807 <hosokawataiki265@gmail.com>
2023-06-15 23:23:01 +0900

    :sparkles: Intercepting Routes

---
+++ apps/view/app/@modal/(.)product/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { ProductModal } from "@/app/_components/pages";
+
+const page = () => {
+  return (
+    <div>
+      <ProductModal />
+    </div>
+  );
+};
+
+export default page;
---
+++ apps/view/app/product/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { ProductModal } from "@/app/_components/pages";
+
+const page = () => {
+  return (
+    <div>
+      page
+      <ProductModal />
+    </div>
+  );
+};
+
+export default page;
