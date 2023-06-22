c6d64b2fa22cd211f8476f22e68b3d9c6379c060
Taiki0807 <hosokawataiki265@gmail.com>
2023-06-23 02:26:52 +0900

    :sparkles: 検索可能なテーブルページを追加

---
+++ apps/view/app/search/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SearchItem from "../_components/pages/SearchItem/SearchItem";
+
+const page = () => {
+  return (
+    <div>
+      <SearchItem />
+    </div>
+  );
+};
+
+export default page;
