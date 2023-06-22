e7f79c3b4696f897bf26d3e68e4eb40b72df7a92
Taiki0807 <hosokawataiki265@gmail.com>
2023-06-23 02:17:36 +0900

    :sparkles: productCardElement追加

--- apps/view/app/shop/page.tsx
+++ apps/view/app/shop/page.tsx
@@ -1,10 +1,13 @@
-import { ProductCard } from "../_components/features";
+import { ProductCard } from "@/app/_components/features";
 
-const page = () => {
+const page = async () => {
+  const productCardElement = await ProductCard();
   return (
     <div className="container max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-      <h2 className="text-2xl font-bold tracking-tight text-gray-900">item</h2>
-      <ProductCard />
+      <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+        Intercepting Routes along with Parallel routes
+      </h2>
+      {productCardElement}
     </div>
   );
 };
