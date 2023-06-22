ae0ac5be408a6c768f50e202bcf27b2971a258b3
Taiki0807 <hosokawataiki265@gmail.com>
2023-06-23 02:33:02 +0900

    :sparkles: 画像ドメインを追加

--- apps/view/next.config.js
+++ apps/view/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, poweredByHeader: false, experimental: { typedRoutes: true } };
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  experimental: { typedRoutes: true },
+  images: {
+    domains: ["unblast.com"],
+  },
+};
 
 module.exports = nextConfig;
