1a9dabc0e2192fce7a074cbea278ec66d46af7ca
Taiki0807 <hosokawataiki265@gmail.com>
2023-08-02 02:39:00 +0900

    :white_check_mark: リクエストのエンドポイント表示

--- apps/view/_mocks/handler.ts
+++ apps/view/_mocks/handler.ts
@@ -1,10 +1,12 @@
 import { rest } from "msw";
 
 export const handlers = [
-  rest.get("http://localhost:8080/api/v1/products", (req, res, ctx) => {
+  rest.get("*/api/v1/products", async (req, res, ctx) => {
+    console.log("Received request:", req);
+
     return res(
       ctx.json({
-        products: [
+        data: [
           { id: 1, name: "Product 1", image: "image1.jpg" },
           { id: 2, name: "Product 2", image: "image2.jpg" },
         ],
