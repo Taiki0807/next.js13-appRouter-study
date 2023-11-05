0f1e8157e6e2b70f214ad4279b2b5f3b02f88db7
Taiki0807 <hosokawataiki265@gmail.com>
2023-11-05 10:48:50 +0900

    💄 デザイン修正

--- apps/view/app/shop/page.tsx
+++ apps/view/app/shop/page.tsx
@@ -1,13 +1,10 @@
-import { ProductCard } from "@/app/_components/features";
+import Shop from "../_components/pages/Shop/Shop";
 
-const page = async () => {
-  const productCardElement = await ProductCard();
+const page = () => {
   return (
-    <div className="container max-w-2xl sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-      <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-        Intercepting Routes along with Parallel routes
-      </h2>
-      {productCardElement}
+    <div>
+      {/* @ts-expect-error Async Server Component */}
+      <Shop />
     </div>
   );
 };
